98b5553af31b61c42dd82a8ab868e27b92ab061d
zhoujing <jing.zhou@terapines.com>
2022-06-28 17:46:02 +0800

    update

--- token.ts
+++ token.ts
@@ -1,4 +1,7 @@
-const str = '.model modelName modelType param1 =val1\n.subckt name type $frfr';
+const str = `.model Q2N2222  (NPN Is="14.34f" Xti=3
++ Itf=.6 Vtf=1.7 Xtf=3 Rb=10)
+v1 net-_r2-pad1_ gnd  dc 10
+q1 net-_c3-pad2_ net-_c1-pad2_ net-_c2-pad2_ Q2N2222`;
 interface CustomizedToken {
     line: number;
     startColumn: number;
@@ -6,19 +9,11 @@ interface CustomizedToken {
     type: string;
     value: string;
 }
-interface TokenConfig {
-    modelDefinition: string;
-    parameterDefinition: string;
-    subcktDefinition: string;
-    deviceDefinition: string;
-    netlistEndDefinition: string,
-    subcktEndDefinition: string
-}
 
 /**
  * TODO: add ads definition here
  */
-const ng_config = require('./ngspice.json') as TokenConfig;
+const ng_config = require('./ngspice.json');
 console.log(ng_config);
 
 /**
@@ -42,7 +37,7 @@ const token = (input: string) => {
         else if (char === '\n' || char === '\r\n') {
             lineNumber++;
             current++
-            columnNumber = 0;
+            columnNumber = 1;
             continue;
         }
         else if (char === '=') {
@@ -56,10 +51,45 @@ const token = (input: string) => {
             current++
             continue;
         }
-        else if (/[a-z\.0-9]/i.test(char) && char !== undefined) {//FIXME:这里后面需要再详细定义具体的
+        else if (new RegExp(ng_config.tokens.leftParenthesis).test(char)) {
+            tokens.push({
+                line: lineNumber,
+                startColumn: columnNumber,
+                endColumn: ++columnNumber,
+                value: char,
+                type: "leftParenthesis"
+            });
+            current++
+            continue;
+        }
+        else if (new RegExp(ng_config.tokens.rightParenthesis).test(char)) {
+            tokens.push({
+                line: lineNumber,
+                startColumn: columnNumber,
+                endColumn: ++columnNumber,
+                value: char,
+                type: "rightParenthesis"
+            });
+            current++
+            continue;
+        }
+        else if (new RegExp(ng_config.tokens.quote).test(char)) {
+            tokens.push({
+                line: lineNumber,
+                startColumn: columnNumber,
+                endColumn: ++columnNumber,
+                value: char,
+                type: "quote"
+            });
+            current++
+            continue;
+        }
+        //这里处理的都是dot commands && string literal, dot command的处理
+        else if (/\./.test(char) && char !== undefined) {
             let _endColumnNumber = columnNumber;
-            let value = '';
-            while (/[a-z\.\=0-9]/i.test(char) && char !== undefined) { //FIXME:这里后面需要再详细定义具体的
+            let value = '.';
+            char = input[++current];
+            while (/[a-z0-9]/i.test(char) && char !== undefined) {
                 value += char;
                 char = input[++current];
                 _endColumnNumber++;
@@ -72,34 +102,68 @@ const token = (input: string) => {
                 type: ""
             }
             //model definition
-            if (new RegExp(ng_config.modelDefinition, 'i').test(value)) {
+            if (new RegExp(ng_config.tokens.model, 'i').test(value)) {
                 tokens.push({
                     ...basicToken,
                     type: "modelDefinition"
                 });
             }
             //subckt definition
-            else if (new RegExp(ng_config.subcktDefinition, 'i').test(value)) {
+            else if (new RegExp(ng_config.tokens.subckt, 'i').test(value)) {
                 tokens.push({
                     ...basicToken,
                     type: "subcktDefinition"
                 });
             }
             //parameter definition
-            else if (new RegExp(ng_config.parameterDefinition, 'i').test(value)) {
+            else if (new RegExp(ng_config.tokens.parameter, 'i').test(value)) {
                 tokens.push({
                     ...basicToken,
                     type: "parameterDefinition"
                 })
             }//这里可以添加任何你想要的token
+
+            //title definition
+            else if (new RegExp(ng_config.tokens.title, 'i').test(value)) {
+                tokens.push({
+                    ...basicToken,
+                    type: "titleDefinition"
+                })
+            }
+
+            //netlistEnd definition
+            else if (new RegExp(ng_config.tokens.netlistEnd, 'i').test(value)) {
+                tokens.push({
+                    ...basicToken,
+                    type: "netlistEndDefinition"
+                })
+            }
+            //subcktEnd definition
+            else if (new RegExp(ng_config.tokens.subcktEnd, 'i').test(value)) {
+                tokens.push({
+                    ...basicToken,
+                    type: "subcktEndDefinition"
+                })
+            }
+
+            //.324f etc number definition
+            else if (/\.\d+/.test(value)) { //这个写的不太好，需要进一步的细分字符类型
+                tokens.push({
+                    ...basicToken,
+                    type: "stringLiteral"
+                })
+            }
+
+            //TODO: Add other token definition here
             else {
                 tokens.push({
                     ...basicToken,
-                    type: 'stringLiteral'
+                    type: 'dotCommand'
                 })
             }
             columnNumber = _endColumnNumber;
         }
+
         else if (char === '$' || char === ';' || char === '*') { //处理注释的逻辑
             let _endColumnNumber = columnNumber;
             let value = '';
@@ -114,6 +178,24 @@ const token = (input: string) => {
                 endColumn: _endColumnNumber,
                 value: value.substring(1).trim(),
                 type: 'comment'//注释
+            })
+            columnNumber = _endColumnNumber;
+        }
+
+        else if (/[\w\d\_\-]/.test(char) && char !== undefined) { //处理注释的逻辑
+            let _endColumnNumber = columnNumber;
+            let value = '';
+            while (/[\w\d\_\-\.]/.test(char) && char !== undefined) { // FIXME:这里后面需要更加详细的定义
+                value += char;
+                char = input[++current];
+                _endColumnNumber++;
+            }
+            tokens.push({
+                line: lineNumber,
+                startColumn: columnNumber,
+                endColumn: _endColumnNumber,
+                value: value,
+                type: 'stringLiteral'
             })
             columnNumber = _endColumnNumber;
         }
